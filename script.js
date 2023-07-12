36e2975892adf621aec4a8029dc66d6d7bdb4c75
eduard <eduard@192-168-0-142.rdsnet.ro>
2023-07-12 09:27:05 +0300

    added testimonial carousel functionality

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const menu = document.querySelector('.phone-menu-container')
 
 const openFaqIcons = document.querySelectorAll('.faq-open')
 
-// Event listeners
+const sliderButtons = document.querySelectorAll('[data-carousel-button]')
+const slides = document.querySelector('[data-slides]')
+const pageValue = document.querySelector('.testimonial-page')
+
+// Event listeners and functions
 // Open phone and tablet menu
 openMenuIcon.forEach(icon =>
   icon.addEventListener('click', () => {
@@ -36,5 +40,26 @@ openFaqIcons.forEach(icon => {
       icon.src = './images/bi_plus.svg'
       icon.parentElement.nextElementSibling.remove()
     }
+  })
+})
+
+// Slide through testimonials and count pages
+sliderButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Assign -1 to prev button and +1 to next button
+    const offset = button.dataset.carouselButton === 'next' ? 1 : -1
+    // Target the active slide
+    const activeSlide = slides.querySelector('[data-active]')
+    // Compute the index of the new slide
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    // Ensure slide continuity when we hit the limits of the carousel's slides
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+    // Make the new slide active
+    slides.children[newIndex].dataset.active = true
+    // Make the previous slide inactive
+    delete activeSlide.dataset.active
+    // Compute the number of slide from total
+    pageValue.textContent = `${newIndex + 1} / ${slides.children.length}`
   })
 })
